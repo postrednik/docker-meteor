7f96fd959a2b70576be415390bc8bbc7ad66058d
Rostislav Postrednik <rostislav@postrednik.cz>
2017-03-22 05:17:13 +0100

    ...

--- latest/scripts/nvm-install.sh
+++ latest/scripts/nvm-install.sh
@@ -7,7 +7,7 @@ nvm ls-remote |
   while IFS= read -r line
   do
     line="$(echo -e "${line}" | tr -d '[[:space:]]')"
-    if [[ $line =~ ^(v)[4,5,6,7].* ]]
+    if [[ $line =~ ^(v)[5,7].* ]]
     then
         line=$(echo $line | tr -c -d 0-9.)
         nvm install $line --reinstall-packages-from=0.10
